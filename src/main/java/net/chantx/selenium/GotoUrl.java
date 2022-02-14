6d3da6ca03f7e1ef02db9b2a64564aa815f1d06c
CHANTXU64 <chantxu@outlook.com>
2022-02-14 13:46:05 +0800

    GotoUrl, Log - add replace arg

--- src/main/java/net/chantx/selenium/GotoUrl.java
+++ src/main/java/net/chantx/selenium/GotoUrl.java
@@ -4,15 +4,13 @@ import java.util.logging.Logger;
 
 import org.openqa.selenium.WebDriver;
 
-//TODO add replace arg in url
-
 public class GotoUrl implements Action {
   private static final Logger LOG = Logger.getLogger(GotoUrl.class.getName());
 
   public String url;
 
   public WebDriver dwork (WebDriver driver, String[] args) {
-    LOG.info("Goto url: " + url);
+    LOG.info("Goto url: " + Args.replace(url, args));
     driver.get(url);
     return driver;
   }
--- src/main/java/net/chantx/selenium/Log.java
+++ src/main/java/net/chantx/selenium/Log.java
@@ -4,15 +4,13 @@ import org.openqa.selenium.WebDriver;
 
 import java.util.logging.Logger;
 
-//TODO add replace arg in info
-
 public class Log implements Action {
   private static final Logger LOG = Logger.getLogger(Log.class.getName());
 
   public String info;
 
   public WebDriver dwork (WebDriver driver, String[] args) {
-    LOG.info("Log: " + info);
+    LOG.info("Log: " + Args.replace(info, args));
     return driver;
   }
 }

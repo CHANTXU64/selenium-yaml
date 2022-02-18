2cf1259107058f52733aed7900f7ee7f1c2b4dd8
CHANTXU64 <chantxu@outlook.com>
2022-02-18 15:15:33 +0800

    change API - Send

--- src/main/java/net/chantx/selenium/Send.java
+++ src/main/java/net/chantx/selenium/Send.java
@@ -8,25 +8,12 @@ public class Send implements Action {
   private static final Logger LOG = Logger.getLogger(Send.class.getName());
 
   public Elem elem;
-  public String[] text;
+  public String text;
 
   public WebDriver dwork (WebDriver driver, String[] args) {
-    assert text.length != 0;
-    String[] s = new String[text.length];
-    for (int i = 0; i < text.length; ++i) {
-      s[i] = Args.replace(text[i], args);
-    }
-    log(s, args);
+    String s = Args.replace(text, args);
+    LOG.info("Send elem: " + elem.getQuery(args) + " - " + s);
     elem.get(driver, args).sendKeys(s);
     return driver;
-  }
-
-  private void log (String[] str, String[] args) {
-    StringBuffer buf = new StringBuffer();
-    buf.append(str[0]);
-    for (int i = 1; i < str.length; ++i) {
-      buf.append(",").append(str[i]);
-    }
-    LOG.info("Send elem: " + elem.getQuery(args) + "\ntext: " + buf);
   }
 }

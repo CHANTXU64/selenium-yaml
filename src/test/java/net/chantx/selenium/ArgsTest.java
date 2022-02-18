104bc333eb8c5fc42fdea4c6f2218bad4192a623
CHANTXU64 <chantxu@outlook.com>
2022-02-18 17:22:45 +0800

    small

--- src/test/java/net/chantx/selenium/ArgsTest.java
+++ src/test/java/net/chantx/selenium/ArgsTest.java
@@ -1,7 +1,6 @@
 package net.chantx.selenium;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
